b30b518b53d030451e86db1b1cb3af50c5746130
Kumaravel <kumaravel1755@gmail.com>
2025-02-24 21:10:46 +0530

    update

--- Agriculture-Management-System/SQL_file/Login.sql
+++
@@ -1,5 +0,0 @@
-create database Login;
-use Login;
-Create table Logininfo(username varchar(20) primary key, password varchar(20));
-insert into Logininfo values("kumar1705",12345);
-select * from Logininfo;
